154fe923cbbe2104b00a5be05cd0a794d980522d
Shami Tomita <shamito@google.com>
2023-10-19 16:17:50 -0400

    updates

--- App.js
+++ App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import WeatherCard from './components/WeatherCard';
+import cities from "./data"
+
 // Import data and WeatherCard here
 
 
@@ -8,10 +11,10 @@ function App() {
             <h1 className = "title">REACTIVE WEATHER</h1>
             <h3 className = "subtitle">Up to the minute weather news</h3>
             <div className = "app">
-                {/* Render components here */}
+                {
+                    cities.map((cityData) => <WeatherCard data={cityData}/>)
+                }
                 
-
-
             </div>
             
         </>
